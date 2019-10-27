2978ed703a6e3283dcb88fd392b413f075bc479c
mbolters <mariematisans@gmail.com>
2019-10-27 14:56:18 -0500

    correcting pathways

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -8,11 +8,11 @@ module.exports = function (app){
   });
 
   app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "./../public/home.html"));
+    res.sendFile(path.join(__dirname, "./../public/index.html"));
   });
   // A default, catch-all route that leads to `home.html` which displays the home page.
 
   app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "./../public/home.html"));
+    res.sendFile(path.join(__dirname, "./../public/index.html"));
   });
 };
