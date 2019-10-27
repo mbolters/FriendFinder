3e2d8d7d93b0ae5bfa1602fdac59afe375ce3748
mbolters <mariematisans@gmail.com>
2019-10-27 17:04:09 -0500

    change pathway in htmlroutes

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -4,10 +4,12 @@ module.exports = function (app){
 
 //   * A GET Route to `/survey` which should display the survey page.
   app.get("/survey", function(req, res) {
+      console.log("A" + res);
     res.sendFile(path.join(__dirname, "../public/survey.html"));
   });
 
   app.get("/", function(req, res) {
+      console.log("B" + res);
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
   // A default, catch-all route that leads to `home.html` which displays the home page.
