bc6ff0202e0bc16f1b5fc41eefa7427bad408702
mbolters <mariematisans@gmail.com>
2019-10-26 22:41:19 -0500

    path fix again

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ app.use(bodyParser.text());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
 
-require("app/routing/apiRoutes")(app);
-require("app/routing/htmlRoutes")(app);
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
