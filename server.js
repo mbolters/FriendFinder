ab1ec23388d44c461a60ca77b192362dd00c713b
mbolters <mariematisans@gmail.com>
2019-10-27 15:43:23 -0500

    correcting pathways

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ var PORT = process.env.PORT || 8080;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.text());
-app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+// app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
 
 require("./app/routing/apiRoutes")(app);
